3d851c9bdbdcb761eb069b45185e0e74c561d3a9
Manuel Mueller <triantium@mailbox.org>
2024-09-11 16:09:00 +0200

    Update bootstrap.js to log headers and modify User-Agent
    Introduced debug logging for request headers and moved User-Agent modification to a conditional block. This change consolidates request headers handling and provides clearer debugging information based on an environment variable.

--- bootstrap.js
+++ bootstrap.js
@@ -2,9 +2,13 @@
 
 const request = require('request-promise');
 const converter = require('rel-to-abs');
+const url = require('url');
 const fs = require('fs');
 const index = fs.readFileSync('index.html', 'utf8');
 const ResponseBuilder = require('./app/ResponseBuilder');
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const debug = process.env.debug_log || false;
 
 module.exports = app => {
     app.get('/*', (req, res) => {
@@ -19,13 +23,16 @@ module.exports = app => {
             res.send(index);
             return;
         }
-
+        if(debug) {
+            console.info("setHeaders origin:" , JSON.stringify(req.headers));
+        }
+        if (req.headers['User-Agent']) {
+            req.headers['User-Agent']= 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+        }
         request({
             uri: requestedUrl,
             resolveWithFullResponse: true,
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
-            }
+            headers: req.headers
         })
         .then(originResponse => {            
             responseBuilder
